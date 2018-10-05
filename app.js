c08ed74ada35dc9b2de674a52e36cfdf7d8efe5c
belqati <nahnahgames@gmail.com>
2018-10-05 18:20:36 -0500

    add form validation example using regExp

--- app.js
+++ app.js
@@ -1800,3 +1800,56 @@ reTest(re, str);
 
 console.clear();
 
+// FORM VALIDATION EXAMPLE
+// form blur event listeners
+document.querySelector('#name').addEventListener('blur', validateName);
+document.querySelector('#zip').addEventListener('blur', validateZip);
+document.querySelector('#email').addEventListener('blur', validateEmail);
+document.querySelector('#phone').addEventListener('blur', validatePhone);
+
+// validation functions
+function validateName(){
+  const name = document.querySelector('#name');
+  const re = /^[a-zA-Z\s\.]{2,10}$/;
+
+  if(!re.test(name.value)){
+    name.classList.add('is-invalid');
+  } else {
+    name.classList.remove('is-invalid');
+  }
+}
+
+function validateZip(){
+  const zip = document.querySelector('#zip');
+  const re = /^[0-9]{5}([- ]?[0-9]{4})?$/;
+
+  if(!re.test(zip.value)){
+    zip.classList.add('is-invalid');
+  } else {
+    zip.classList.remove('is-invalid');
+  }
+}
+
+function validateEmail(){
+  const email = document.querySelector('#email');
+  // const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+  const re = /^([\w.-]+)@([\w.-]+)\.(\w\D{2,5})$/;
+
+  if(!re.test(email.value)){
+    email.classList.add('is-invalid');
+  } else {
+    email.classList.remove('is-invalid');
+  }
+}
+
+function validatePhone(){
+  const phone = document.querySelector('#phone');
+  const re = /^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/;
+
+  if(!re.test(phone.value)){
+    phone.classList.add('is-invalid');
+  } else {
+    phone.classList.remove('is-invalid');
+  }
+}
+
